35276869b5c80a81a0533fa693e843049b50ca6d
Suado Cowboy <luccacsg@gmail.com>
2024-03-09 19:58:13 -0300

    fixed some weird error that I could not compile(I don't understand java :sob:)

--- src/main/java/com/luccarieffel/interpreter/command/ICommandRunFunc.java
+++
@@ -1,8 +0,0 @@
-package com.luccarieffel.interpreter.command;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface ICommandRunFunc {
-    void run(String name, int minArgs, int maxArgs, String usage, List<String> args);
-}
